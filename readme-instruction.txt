f9f12e1a4827d6fe527922ab75d0beff45c5b302
Dev Singh <dev.thep@hotmail.com>
2017-02-03 22:46:22 +0700

    readme-instruction.txt done

---
+++ readme-instruction.txt
@@ -0,0 +1,33 @@
+MANUAL
+1. go {direction}
+	-{direction} north, south, east, west
+2. take {item}
+	- if item is present in the room, and it does not
+	exceed your inventory then you can pick it up
+3. take {item} from {container}
+	-containers are objects that contain items
+	-must use this to pick up objects from {container}
+4. open {container}
+	-before picking up things from {container}
+	must open it first
+	-also reveals the content of the bag.
+	-if the bag is open, can use this command
+	to see what's inside.
+5. attack {weapon}
+	- if the monster is in the room, the player will 
+	automatically attack. Just have to specify weapon to use
+6. drop {item}
+	- drop item
+7. use {potion}
+	- use potion items that can heal your player
+8. read
+	- almost every room has a note, read it to uncover
+	some mysteries
+9. quit
+	- quit game
+10. info
+	- reveals player stats and room info.
+11. info player
+	- reveals room info
+12. info room
+	- reveals player info
--- src/main/java/io/muic/ooc/zork/ZorkGame.java
+++ src/main/java/io/muic/ooc/zork/ZorkGame.java
@@ -1,8 +1,64 @@
 package io.muic.ooc.zork;
 
+import io.muic.ooc.zork.command.*;
+import io.muic.ooc.zork.map.GameMap;
+import io.muic.ooc.zork.map.GameMapGenerator;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ZorkGame {
     public void start(){
+        GameMapGenerator generator = new GameMapGenerator();
+        GameMap map = generator.createLvl1();
+        GameMap map2 = generator.createLvl2();
+        Player p1 = new Player("Dev",map.get(1));
+        p1.setMap(map);
+        map.setNextMap(map2);
+        HashMap<String, Command> commands = new HashMap<String, Command>() {
+            {
+                // commands are added here using lambdas. It is also possible to dynamically add commands without editing the code.
+                put("go", new GoCommand(p1));
+                put("take", new TakeCommand(p1));
+                put("open", new OpenCommand(p1));
+                put("info", new InfoCommand(p1));
+                put("attack", new AttackCommand(p1));
+                put("drop", new DropCommand(p1));
+                put("use", new UseCommand(p1));
+                put("read", new ReadCommand(p1));
+                put("quit", new QuitCommand(p1));
+
+            }
+        };
+        boolean quit = false;
+        Scanner in = new Scanner(System.in);
+        System.out.print("> ");
+        String line = in.nextLine();
+        while (!quit){
+            String cmd = line.trim().split(" ")[0].toLowerCase();
+            try{
+                commands.get(cmd).apply(new Scanner(line));
+            }catch (NullPointerException e){
+                System.out.println("Invalid input");
+            }
+            if (p1.getCurrentRoom().containMonster()){
+                Monster m = p1.getCurrentRoom().getMonster();
+                if (m.isAlive()){
+                    m.attack(p1);
+                    System.out.println(m.getName() + " attacked you. HP remaining : " + p1.getHP());
+                }else{
+                    System.out.println("Mors is DEAD");
+                    break;
+                }
+            }if (p1.getHP() < 0){
+                System.out.println("YOU LOST -> GAME OVER!!!");
+                break;
+            }
+            System.out.print("> ");
+            line = in.nextLine();
+        }
+        if (p1.getHP()>= 0){
+            generator.createLvl3();
+        }
     }
 }
